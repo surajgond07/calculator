3e39d387ed93dfc48c1e96b89c65d47143109e8f
Suraj Gond <surajgond07@gmail.com>
2024-05-22 14:42:26 +0530

    Creating Table row

---
+++ android/app/src/main/kotlin/com/surajinfotech/calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.surajinfotech.calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SimpleCalculator(),
+    );
+  }
+}
+
+class SimpleCalculator extends StatefulWidget {
+  const SimpleCalculator({super.key});
+
+  @override
+  State<SimpleCalculator> createState() => _SimpleCalculatorState();
+}
+
+class _SimpleCalculatorState extends State<SimpleCalculator> {
+  // Method for Button
+  Widget buildButton(
+      String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: TextButton(
+        style: ButtonStyle(
+          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0),
+              side: const BorderSide(
+                color: Colors.white,
+                width: 1,
+                style: BorderStyle.solid,
+              ),
+            ),
+          ),
+          padding: WidgetStateProperty.all<EdgeInsets>(
+            const EdgeInsets.all(16.0),
+          ),
+        ),
+        onPressed: () {
+          // on press logic
+        },
+        child: Text(
+          buttonText,
+          style: const TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Calculator',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 38.0,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 48.0,
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Divider(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  width: MediaQuery.of(context).size.width * .75,
+                  color: Colors.redAccent,
+                  child: Table(
+                    children: [
+                      TableRow(
+                        children: [
+                          // unicode symbol
+                          // https://www.fileformat.info/info/unicode/category/Sm/list.htm
+                          buildButton("C", 1, Colors.redAccent),
+                          buildButton("⌫", 1, Colors.blue),
+                          buildButton("÷", 1, Colors.blue),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          // unicode symbol
+                          // https://www.fileformat.info/info/unicode/category/Sm/list.htm
+                          buildButton("7", 1, Colors.black54),
+                          buildButton("8", 1, Colors.black54),
+                          buildButton("9", 1, Colors.black54),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          // unicode symbol
+                          // https://www.fileformat.info/info/unicode/category/Sm/list.htm
+                          buildButton("4", 1, Colors.black54),
+                          buildButton("5", 1, Colors.black54),
+                          buildButton("6", 1, Colors.black54),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          // unicode symbol
+                          // https://www.fileformat.info/info/unicode/category/Sm/list.htm
+                          buildButton("1", 1, Colors.black54),
+                          buildButton("2", 1, Colors.black54),
+                          buildButton("3", 1, Colors.black54),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          // unicode symbol
+                          // https://www.fileformat.info/info/unicode/category/Sm/list.htm
+                          buildButton(".", 1, Colors.black54),
+                          buildButton("0", 1, Colors.black54),
+                          buildButton("00", 1, Colors.black54),
+                        ],
+                      ),
+                    ],
+                  )),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
